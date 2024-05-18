c50e4e57fe62090fa6fcddf6c75da950bb720748
moualhi zine el abidine <zmoualhi@outlook.com>
2024-05-18 20:15:25 +0100

    fix 🐛: merge conflicts
    Signed-off-by: moualhi zine el abidine <zmoualhi@outlook.com>

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -4,3 +4,4 @@ export const PAGE_SIZE = 8
 
 export const LIGHT_MODE = 'light', DARK_MODE = 'dark', AUTO_MODE = 'auto'
 export const DEFAULT_THEME = AUTO_MODE
+export const SITE_URL = 'https://blog.zine.casa';
